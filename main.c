dc75a89ab6559bd9b88566750b3a7c62b75a68af
rykov@rline.pro <rykov@rline.pro>
2024-11-20 16:34:49 +0500

    version 1

--- main.c
+++ main.c
@@ -9,30 +9,26 @@ int main()
     int row_count = cells;
     int col_count = cells + 1;
 
-    const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
     int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
 
     // Горизонтальные линии
-    line(col_count, "┌", "───┬", "───┐\n");
+    line(col_count, "┌", "───┬", NULL,"───┐\n");
 
     //вертикальные линии
-    printf("│   │");
-    line2(col_count, letters);
-    printf("\n");
+    line(col_count, "│   │", NULL, row_header, "\n");
 
     for (int i = 0; i < row_count; i++)
     {
         // Горизонтальные линии
-        line(col_count, "├", "───┼", "───┤\n");
+        line(col_count, "├", "───┼", NULL,"───┤\n");
 
         // Вертикальные разделители
-        printf("│%2d │", numbers[i]);
-
-        line(col_count, "", "   │", "\n");
+        line(col_count, bprintf("│%2d │", numbers[i]),"   │",  NULL, "\n");
     }
                      
     // Горизонтальные линии
-    line(col_count, "└", "───┴", "───┴\n");
+    line(col_count, "└", "───┴", NULL,"───┴\n");
 
     return 0;
 }
@@ -49,20 +45,23 @@ char *bprintf(char *fmt, ...)
     return buffer;
 }
 
-void line2(int cols, const char *letters[10])
-{
-    for (int j = 0; j < cols - 1; j++)
-    {
-        printf(" %s │", letters[j]);
-    }
-}
-
-void line(int cols, const char *start, const char *chars, const char *end)
+void line(int cols, const char *start, const char *s, void (chars(int i)), const char *end)
 {
     printf(start);
     for (int j = 0; j < cols - 1; j++)
     {
-        printf(chars);
+        if (NULL != s)
+            printf(s);
+        if (NULL != chars)
+            chars(j);
     }
     printf(end);
+}
+
+
+void row_header(int j)
+{
+    const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
+
+    printf(" %s │", letters[j]);
 }
