c03ee11661d900ffe697543950141f1cf7d7168b
NatalinaR <rykova.natali@gmail.com>
2025-05-14 12:52:58 +0200

    refractored

--- main.c
+++ main.c
@@ -1,60 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "main.h"
 #include <stdarg.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <string.h>
 
-#define MAX_ROWS 10
-#define MAX_COLS 10
-#define SHIPS_NUMBER 5
+#define MAX_ROWS 5
+#define MAX_COLS 5
+#define SHIPS_NUMBER 2
 
-// Глобальная матрица для хранения маркеров
-char grid_destryed_ships[2][MAX_COLS][MAX_ROWS];
+#define SHIP_CHARACTER '█'
+#define MISS_CHARACTER '+'
+#define DESTROYED_SHIP_CHARACTER '░'
+
+// matrix to save markers
 char grid_visible[2][MAX_COLS][MAX_ROWS];
-char grid_chosen_cells[2][MAX_COLS][MAX_ROWS];
 
-// Инициализация глобальной матрицы
+#pragma region Main
 void initialize_grid()
 {
-    memset(grid_destryed_ships, ' ', sizeof(grid_destryed_ships));
     memset(grid_visible, ' ', sizeof(grid_visible));
-    memset(grid_chosen_cells, ' ', sizeof(grid_chosen_cells));
 }
 
 int main()
 {
-    initialize_grid(); // matrix initialisation
-
-    // add_ship(0, 0, 2, 2);
-    const int cells = 10;
+    char choice;
     char s;
 
-    int rows = cells;
-    int cols = cells + 1;
-    const int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    do
+    {
+        initialize_grid();
+        int rows = MAX_ROWS;
+        int cols = MAX_COLS + 1;
+        const int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-    // int current_player = ask_player_number();
-    // printf("Выбран игрок %d\n", current_player);
-    // place_all_ships(current_player);
-    printf("Player 1, place your ship:\n");
-    place_all_ships(1);
+        printf("Player 1, place your ship:\n");
+        place_all_ships(1);
 
-    printf("Player 2, place your ship:\n");
-    place_all_ships(2);
+        printf("Player 2, place your ship:\n");
+        place_all_ships(2);
 
-    // draw square
+        game_loop(cols, rows, numbers);
 
-    // process_input(&ship, current_player);
+        printf("Good GAME!\n");
 
-    game_loop(cols, rows, numbers);
+        printf("Do you want to play again? (y/n): ");
+        scanf(" %c", &choice);
 
-    printf("No Ships Left!\n");
+        system("cls");
+    } while (choice == 'y' || choice == 'Y');
+
+    printf("Thank you for playing!\n");
     scanf("%s", &s);
     return 0;
 }
+#pragma endregion
+
+#pragma region Board
 
 char *bprintf(char *fmt, ...)
 {
@@ -80,12 +83,72 @@ void line(int cols, const char *start, const char *middle, void(chars(int i)), c
     printf("%s%s", end, offset != 0 ? "\n" : "");
 }
 
+void line_with_markers(int cols, const char *start, const char *middle, void(chars(int i)), const char *end, int offset, int row, int player_index)
+{
+    printf("%*s%s", offset, "", start);
+
+    for (int j = 0; j < cols - 1; j++)
+    {
+        char cell = grid_visible[offset / 3][j][row];
+        bool show_S = (player_index == 1 && offset == 0) || (player_index == 2 && offset == 3);
+
+        switch (cell)
+        {
+        case DESTROYED_SHIP_CHARACTER:
+            printf(" %c │", DESTROYED_SHIP_CHARACTER);
+            break;
+        case MISS_CHARACTER:
+            printf(" %c │", MISS_CHARACTER);
+            break;
+        case SHIP_CHARACTER:
+            if (show_S)
+                printf(" %c │", SHIP_CHARACTER);
+            else
+                printf("   │");
+            break;
+        default:
+            printf("   │");
+            break;
+        }
+
+        if (chars != NULL)
+            chars(j);
+    }
+
+    printf("%s%s", end, offset != 0 ? "\n" : "");
+}
+
 void row_header(int j)
 {
     const char *letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
     printf(" %s │", letters[j]);
 }
 
+void draw_boards(int cols, int rows, const int *numbers, int player_index)
+{
+    line(cols, "┌", "───┬", NULL, "───┐", 0);
+    line(cols, "┌", "───┬", NULL, "───┐", 3);
+
+    line(cols, "│   │", NULL, row_header, "", 0);
+    line(cols, "│   │", NULL, row_header, "", 3);
+    
+    for (int i = 0; i < rows; i++)
+    {
+
+        line(cols, "├", "───┼", NULL, "───┤", 0);
+        line(cols, "├", "───┼", NULL, "───┤", 3);
+        
+        line_with_markers(cols, bprintf("│%2d │", numbers[i]), "   │", NULL, "", 0, i, player_index);
+        line_with_markers(cols, bprintf("│%2d │", numbers[i]), "   │", NULL, "", 3, i, player_index);
+    }
+
+    line(cols, "└", "───┴", NULL, "───┘", 0);
+    line(cols, "└", "───┴", NULL, "───┘", 3);
+}
+
+#pragma endregion
+
+#pragma region Input
 void get_coordinates(const char *input, int *row, int *col)
 {
     if (input == NULL || input[0] == '\0' || input[1] == '\0')
@@ -122,20 +185,17 @@ void get_coordinates(const char *input, int *row, int *col)
     }
 }
 
-void process_input(int *ship_count, int main_player)
+void process_input(int *ship_count, int main_player, const int *numbers)
 {
     char input[10];
     int row, col;
 
     int enemy = main_player == 1 ? 2 : 1;
 
-    // int current_player = ask_player_number();
-    // printf("Выбран игрок %d\n", current_player);
-    // place_all_ships(current_player);
     while (1)
     {
 
-        printf("Attack (e.g., A2 or J10): ");
+        printf("Attack (e.g., A2): ");
         scanf("%s", input);
         get_coordinates(input, &row, &col);
 
@@ -147,19 +207,23 @@ void process_input(int *ship_count, int main_player)
         }
 
         // Check if already chosen
-        if (is_cell_chosen(row - 1, col - 1, enemy))
+        if (check_cell(row - 1, col - 1, enemy, 'C'))
         {
             printf("Cell %d,%d already has been chosen, pick another one\n", row, col);
             continue;
         }
 
         // If occupied (hit)
-        if (is_cell_occupied(row - 1, col - 1, enemy))
+        if (check_cell(row - 1, col - 1, enemy, SHIP_CHARACTER))
         {
-            add_marker(row - 1, col - 1, enemy);
+            system("cls");
+            add_marker(row - 1, col - 1, enemy, DESTROYED_SHIP_CHARACTER);
+            draw_boards(MAX_COLS + 1, MAX_ROWS, numbers, main_player);
+
+
             printf("HIT!\n");
             (*ship_count)--;
-            printf("Ships left: %d\n", *ship_count);
+            // printf("Ships left: %d\n", *ship_count);
 
             // Allow another shot if it was a hit
             if (*ship_count == 0)
@@ -167,209 +231,37 @@ void process_input(int *ship_count, int main_player)
         }
         else
         {
-            add_miss(row - 1, col - 1, enemy);
+            system("cls");
+            add_marker(row - 1, col - 1, enemy, MISS_CHARACTER);
+            draw_boards(MAX_COLS + 1, MAX_ROWS, numbers, main_player);
             printf("MISS.\n");
-            break; // Exit loop on miss
+            break;
         }
     }
 }
+#pragma endregion
 
-// Функция для добавления маркера
-
-bool add_ship(int j, int i, int player)
-{
-    int offset = player == 1 ? 0 : 3;
-
-    // Проверка допустимости индексов и игрока
-    if (j < 0 || j >= MAX_COLS || i < 0 || i >= MAX_ROWS || (player != 1 && player != 2))
-    {
-        printf("Wrong marker parameters!\n");
-        return false;
-    }
-
-    // В зависимости от игрока, добавляем в нужную сетку
-    grid_visible[offset / 3][i][j] = 'K';
-
-    return true;
-}
-
-bool add_marker(int j, int i, int player)
-{
-    int offset = player == 1 ? 0 : 3;
-
-    // Проверка допустимости индексов и игрока
-    if (j < 0 || j >= MAX_COLS || i < 0 || i >= MAX_ROWS || (player != 1 && player != 2))
-    {
-        printf("Wrong marker parameters!\n");
-        return false;
-    }
-    grid_destryed_ships[offset / 3][i][j] = 'D';
-
-    return true;
-}
-
-bool add_miss(int j, int i, int player)
-{
-    int offset = player == 1 ? 0 : 3;
-
-    // Проверка допустимости индексов и игрока
-    if (j < 0 || j >= MAX_COLS || i < 0 || i >= MAX_ROWS || (player != 1 && player != 2))
-    {
-        printf("Wrong marker parameters!\n");
-        return false;
-    }
-    grid_chosen_cells[offset / 3][i][j] = '+';
-
-    return true;
-}
-
-// Функция для проверки наличия маркера в клетке
-bool is_cell_occupied(int j, int i, int player)
-{
-    int offset = player == 1 ? 0 : 3;
-
-    // Проверка допустимости индексов
-    if (j < 0 || j >= MAX_COLS || i < 0 || i >= MAX_ROWS || (player != 1 && player != 2))
-    {
-        printf("Wrong marker parameters!\n");
-        return false;
-    }
-
-    // Проверка, занята ли клетка
-    return grid_visible[offset / 3][i][j] == 'K';
-}
-
-// checking has cell been already chosen once
-bool is_cell_chosen(int j, int i, int player)
-{
-    int offset = player == 1 ? 0 : 3;
-
-    // Проверка допустимости индексов
-    if (j < 0 || j >= MAX_COLS || i < 0 || i >= MAX_ROWS || (player != 1 && player != 2))
-    {
-        printf("Wrong marker parameters!\n");
-        return false;
-    }
-
-    // check was chosen or not
-    return grid_chosen_cells[offset / 3][i][j] == '+';
-}
-
-// Обновленная функция line с учетом маркеров
-void line_with_markers(int cols, const char *start, const char *middle, void(chars(int i)), const char *end, int offset, int row, int player_index)
-{
-    printf("%*s%s", offset, "", start);
-    for (int j = 0; j < cols - 1; j++)
-    {
-        if (player_index == 1 && offset == 0)
-        {
-            if (grid_visible[offset / 3][j][row] == 'K' && grid_destryed_ships[offset / 3][j][row] == 'D')
-            {
-                printf(" D │");
-            }
-            else if (grid_visible[offset / 3][j][row] == 'K' && grid_destryed_ships[offset / 3][j][row] != 'D')
-            {
-                printf(" K │");
-            }
-            else if (grid_chosen_cells[offset / 3][j][row] == '+')
-            {
-
-                printf(" + │");
-            }
-            else
-            {
-                printf("   │");
-            }
-            if (NULL != chars)
-                chars(j);
-        }
-        else if (player_index == 2 && offset == 3)
-        {
-            if (grid_visible[offset / 3][j][row] == 'K' && grid_destryed_ships[offset / 3][j][row] == 'D')
-            {
-                printf(" D │");
-            }
-            else if (grid_visible[offset / 3][j][row] == 'K' && grid_destryed_ships[offset / 3][j][row] != 'D')
-            {
-                printf(" K │");
-            }
-            else if (grid_chosen_cells[offset / 3][j][row] == '+')
-            {
-
-                printf(" + │");
-            }
-            else
-            {
-                printf("   │");
-            }
-            if (NULL != chars)
-                chars(j);
-        }
-        else if (player_index == 0)
-        {
-            if (grid_visible[offset / 3][j][row] == 'K' && grid_destryed_ships[offset / 3][j][row] == 'D')
-            {
-                printf(" D │");
-            }
-            else if (grid_visible[offset / 3][j][row] == 'K' && grid_destryed_ships[offset / 3][j][row] != 'D')
-            {
-                printf(" K │");
-            }
-            else if (grid_chosen_cells[offset / 3][j][row] == '+')
-            {
-
-                printf(" + │");
-            }
-            else
-            {
-                printf("   │");
-            }
-            if (NULL != chars)
-                chars(j);
-        }
-        else
-        {
-            if (grid_visible[offset / 3][j][row] == 'K' && grid_destryed_ships[offset / 3][j][row] == 'D')
-            {
-                printf(" D │");
-            }
-            else if (grid_chosen_cells[offset / 3][j][row] == '+')
-            {
-
-                printf(" + │");
-            }
-            else
-            {
-                printf("   │");
-            }
-            if (NULL != chars)
-                chars(j);
-        }
-    }
-    printf("%s%s", end, offset != 0 ? "\n" : "");
-}
-
+#pragma region GameLogic
 void place_all_ships(int player)
 {
     int ships_to_place = SHIPS_NUMBER;
     char input[10];
     int row, col;
-    // int board_owner = player;
 
-    printf("=== РPlacing ships for player %d ===\n", player);
+    printf("=== Рlacing ships for player %d ===\n", player);
 
     for (int i = 0; i < ships_to_place;)
     {
-        printf("Place a ship #%d (e.g, A2 or J10): ", i + 1);
+        printf("Place a ship #%d (e.g, A2): ", i + 1);
         scanf("%s", input);
         get_coordinates(input, &row, &col);
 
         if (row != -1 && col != -1)
         {
-            if (add_ship(row - 1, col - 1, player)) // unified function
+            if (add_marker(row - 1, col - 1, player, SHIP_CHARACTER))
             {
-                printf("Ship #%d placed to %s\n", i + 1, input);
-                i++; // only increment if successful
+                // printf("Ship #%d placed to %s\n", i + 1, input);
+                i++;
             }
             else
             {
@@ -382,27 +274,12 @@ void place_all_ships(int player)
         }
     }
 
-    printf("All %d ships have been placed for plazer %d.\n", ships_to_place, player);
-    getchar();
+    printf("All %d ships have been placed for player %d.\n", ships_to_place, player);
+
+    printf("\n===== Game LOADING =====\n");
+
+    Sleep(1000);
     system("cls");
-}
-
-int ask_player_number()
-{
-    int player;
-
-    do
-    {
-        printf("Are you player 1 or 2? Enter 1 or 2: ");
-        scanf("%d", &player);
-
-        if (player != 1 && player != 2)
-        {
-            printf("Please repeat again\n");
-        }
-    } while (player != 1 && player != 2);
-
-    return player;
 }
 
 void game_loop(int cols, int rows, const int *numbers)
@@ -413,49 +290,97 @@ void game_loop(int cols, int rows, const int *numbers)
     while (ships_player1 > 0 && ships_player2 > 0)
     {
         printf("\n===== TURN PLAYER 1 =====\n");
+
         draw_boards(cols, rows, numbers, 1);
-        process_input(&ships_player2, 1); // Игрок 1 атакует игрока 2
+
+       
+        process_input(&ships_player2, 1, numbers); // player 1 attack player 2
 
         if (ships_player2 == 0)
         {
             printf("Player 1 won!\n");
-            draw_boards(cols, rows, numbers, 0);
             break;
         }
 
+        printf("\n===== Next player loading =====\n");
+        Sleep(2000);
+
         system("cls");
         printf("\n===== TURN PLAYER 2 =====\n");
+
         draw_boards(cols, rows, numbers, 2);
-        process_input(&ships_player1, 2); // Игрок 2 атакует игрока 1
+
+        process_input(&ships_player1, 2, numbers); // player 2 attack player 1
 
         if (ships_player1 == 0)
         {
             printf("Player 2 won!\n");
-            draw_boards(cols, rows, numbers, 0);
             break;
         }
+
+        printf("\n===== Next player loading =====\n");
+        Sleep(2000);
         system("cls");
     }
 }
+#pragma endregion
 
-void draw_boards(int cols, int rows, const int *numbers, int player_index)
+#pragma region GridHelpers
+bool add_marker(int j, int i, int player, char purpose)
 {
-    line(cols, "┌", "───┬", NULL, "───┐", 0);
-    line(cols, "┌", "───┬", NULL, "───┐", 3);
+    int offset = player == 1 ? 0 : 3;
 
-    line(cols, "│   │", NULL, row_header, "", 0);
-    line(cols, "│   │", NULL, row_header, "", 3);
-
-    for (int i = 0; i < rows; i++)
+    // Проверка допустимости индексов и игрока
+    if (j < 0 || j >= MAX_COLS || i < 0 || i >= MAX_ROWS || (player != 1 && player != 2))
     {
-
-        line(cols, "├", "───┼", NULL, "───┤", 0);
-        line(cols, "├", "───┼", NULL, "───┤", 3);
-
-        line_with_markers(cols, bprintf("│%2d │", numbers[i]), "   │", NULL, "", 0, i, player_index);
-        line_with_markers(cols, bprintf("│%2d │", numbers[i]), "   │", NULL, "", 3, i, player_index);
+        printf("Wrong marker parameters!\n");
+        return false;
     }
 
-    line(cols, "└", "───┴", NULL, "───┘", 0);
-    line(cols, "└", "───┴", NULL, "───┘", 3);
+    switch (purpose)
+    {
+    case SHIP_CHARACTER:
+        grid_visible[offset / 3][i][j] = SHIP_CHARACTER;
+        break;
+
+    case DESTROYED_SHIP_CHARACTER:
+        grid_visible[offset / 3][i][j] = DESTROYED_SHIP_CHARACTER;
+        break;
+
+    case MISS_CHARACTER:
+        grid_visible[offset / 3][i][j] = MISS_CHARACTER;
+        break;
+
+    default:
+        printf("Unknown purpose type: %c\n", purpose);
+        return false;
+    }
+
+    return true;
 }
+
+bool check_cell(int j, int i, int player, char type)
+{
+    int offset = player == 1 ? 0 : 3;
+
+    if (j < 0 || j >= MAX_COLS || i < 0 || i >= MAX_ROWS || (player != 1 && player != 2))
+    {
+        printf("Wrong marker parameters!\n");
+        return false;
+    }
+
+    char cell = grid_visible[offset / 3][i][j];
+
+    switch (type)
+    {
+    case SHIP_CHARACTER: // check if a ship is present
+        return cell == SHIP_CHARACTER;
+    case 'C': // check if cell is chosen (hit or miss)
+        return cell == MISS_CHARACTER || cell == DESTROYED_SHIP_CHARACTER;
+    default:
+        printf("Unknown check type: %c\n", type);
+        return false;
+    }
+}
+
+#pragma endregion
--- main.h
+++ main.h
@@ -1,34 +1,22 @@
 #pragma once
 #include <stdbool.h>
-void line(int cols, const char *start, const char *middle, void (chars(int i)), const char *end, int offset);
 
-
-
-// void first_line(int cols);
-
-// void first_vertical_line(int cols, const char *letters[10]);
-
-// void horizontal_line_field(int cols);
-
-// void vertical_line_field(int cols, int numbers[10], int i);
-
-// void last_line(int cols);
-
+void initialize_grid();
 char *bprintf(char *fmt, ...);
 void row_header(int j); 
-void get_coordinates(const char *input, int *row, int *col);
-void process_input(int *ship_count, int board_owner);
-
+void draw_boards(int cols, int rows, const int *numbers, int player_index);
+void line(int cols, const char *start, const char *middle, void (chars(int i)), const char *end, int offset);
 void line_with_markers(int cols, const char *start, const char *middle, void(chars(int i)), const char *end, int offset, int row, int player_index);
 
-bool is_cell_occupied(int j, int i, int player);
-bool is_cell_chosen(int j, int i, int player);
-// bool add_ship(int j, int i, int player, int board_owner);
-bool add_ship(int j, int i, int player);
-bool add_marker(int j, int i, int player);
-bool add_miss(int j, int i, int player);
-int ask_player_number();
 void place_all_ships(int player);
-void draw_boards(int cols, int rows, const int *numbers, int player_index);
+void get_coordinates(const char *input, int *row, int *col);
+
+void process_input(int *ship_count, int board_owner, const int *numbers);
 void game_loop(int cols, int rows, const int *numbers);
+
+bool check_cell(int j, int i, int player, char type);
+bool add_marker(int j, int i, int player, char purpose);
+
+
+
 
